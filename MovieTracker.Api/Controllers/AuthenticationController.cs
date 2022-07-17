22f1ad12ffba178da76875f061e4f21f158d513b
Vintage Trouble <vadim.akh42@gmail.com>
2022-07-17 13:27:29 +0700

    Use service for controller

--- MovieTracker.Api/Controllers/AuthenticationController.cs
+++ MovieTracker.Api/Controllers/AuthenticationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using MovieTracker.Api.Controllers.Abstract;
+using MovieTracker.Application.Services.Authentication;
 using MovieTracker.Contracts.Authentication;
 
 namespace MovieTracker.Api.Controllers;
@@ -8,16 +9,47 @@ namespace MovieTracker.Api.Controllers;
 [Route("api/auth")]
 public class AuthenticationController : BaseController
 {
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest request)
     {
-        return Ok(request);
+        var authResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
     }
 
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request)
     {
-        return Ok(request);
+        var authResult = _authenticationService.Login(
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
     }
 }
 
