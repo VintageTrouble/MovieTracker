20cbcbe7924d3157c84ede60645a221f4b36c1f6
Vintage Trouble <vadim.akh42@gmail.com>
2022-07-17 13:02:14 +0700

    Add endpoints

---
+++ MovieTracker.Api/Controllers/Abstract/BaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieTracker.Api.Controllers.Abstract;
+
+[ApiController]
+public class BaseController : ControllerBase
+{
+}
---
+++ MovieTracker.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MovieTracker.Api.Controllers.Abstract;
+using MovieTracker.Contracts.Authentication;
+
+namespace MovieTracker.Api.Controllers;
+
+[Route("api/auth")]
+public class AuthenticationController : BaseController
+{
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok(request);
+    }
+}
+
