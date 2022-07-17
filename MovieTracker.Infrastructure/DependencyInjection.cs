85ee2ff50ef06bce9cae425963c74798a6db0cfd
Vintage Trouble <vadim.akh42@gmail.com>
2022-07-17 15:04:32 +0700

    Add Jwt generation

--- MovieTracker.Infrastructure/DependencyInjection.cs
+++ MovieTracker.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-using MovieTracker.Application.Services.Authentication;
+using MovieTracker.Application.Common.Interfaces.Authentication;
+using MovieTracker.Application.Common.Interfaces.Services;
+using MovieTracker.Infrastructure.Authentication;
+using MovieTracker.Infrastructure.Services;
 
 namespace MovieTracker.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
-        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
         return services;
     }
---
+++ MovieTracker.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using MovieTracker.Application.Common.Interfaces.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
+
