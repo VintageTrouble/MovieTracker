817869f0dd39f3c6a123e040b9cde03bfda5b85f
Vintage Trouble <vadim.akh42@gmail.com>
2022-07-17 11:49:22 +0700

    Startup + Swagger configurations

---
+++ MovieTracker.Api/Swagger/StartupSwaggerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace MovieTracker.Api.Swagger
+{
+    public static class StartupSwaggerConfiguration
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public static void ConfigueSwagger(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+        }
+    }
+}
