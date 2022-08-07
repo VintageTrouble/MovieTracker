2dc83a5284dcb9b55a609e09b3d844e7ff1d0e7d
Vintage Trouble <vadim.akh42@gmail.com>
2022-08-07 19:46:30 +0700

    Implement "CreateValidationProblemDetails" method for "ProblemDetailsFactory"

--- MovieTracker.Api/Common/Errors/MovieTrackerProblemDetailsFactory.cs
+++ MovieTracker.Api/Common/Errors/MovieTrackerProblemDetailsFactory.cs
@@ -39,9 +39,37 @@ public class MovieTrackerProblemDetailsFactory : ProblemDetailsFactory
         return problemDetails;
     }
 
-    public override ValidationProblemDetails CreateValidationProblemDetails(HttpContext httpContext, ModelStateDictionary modelStateDictionary, int? statusCode = null, string? title = null, string? type = null, string? detail = null, string? instance = null)
+    public override ValidationProblemDetails CreateValidationProblemDetails(
+        HttpContext httpContext,
+        ModelStateDictionary modelStateDictionary,
+        int? statusCode = null,
+        string? title = null,
+        string? type = null,
+        string? detail = null,
+        string? instance = null)
     {
-        throw new NotImplementedException();
+        if(modelStateDictionary is null)
+            throw new ArgumentNullException(nameof(modelStateDictionary));
+
+        statusCode ??= 400;
+
+        var problemDetails = new ValidationProblemDetails(modelStateDictionary)
+        {
+            Status = statusCode,
+            Type = type,
+            Detail = detail,
+            Instance = instance,
+        };
+
+        if (title != null)
+        {
+            // For validation problem details, don't overwrite the default title with null.
+            problemDetails.Title = title;
+        }
+
+        ApplyProblemDetailsDefaults(httpContext, problemDetails, statusCode.Value);
+
+        return problemDetails;
     }
 
     private void ApplyProblemDetailsDefaults(HttpContext httpContext, ProblemDetails problemDetails, int statusCode)
